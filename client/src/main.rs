b8aa2045a68d8a87de5f80964e389e213dc6942a
Anthony Cramp <anthony.cramp@gmail.com>
2021-11-07 00:40:05 +1030

    Tried to add some messaging

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,7 +4,10 @@ use std::net::TcpStream;
 fn main() -> std::io::Result<()> {
     println!("Hello, client!");
     let mut stream = TcpStream::connect("127.0.0.1:33333")?;
-    stream.write(&[1])?;
+    println!("Local address: {:?}", stream.local_addr()?);
+    println!("Peer address: {:?}", stream.peer_addr()?);
+    let message = "hello, server. I'm client";
+    stream.write(message.as_bytes())?;
 
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,14 @@
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::str::from_utf8;
+
+fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
+    println!("{:?}", stream.peer_addr()?);
+    let mut buf: Vec<u8> = vec![];
+    stream.read(&mut buf)?;
+    println!("Received message: {:?}", from_utf8(&buf));
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     println!("Hello, server!");
@@ -6,7 +16,7 @@ fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:33333")?;
 
     for stream in listener.incoming() {
-        println!("{:?}", stream?.peer_addr()?);
+        handle_client(&mut stream?);
     }
 
     Ok(())
